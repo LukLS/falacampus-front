2c6aa4f6cbe7574b1649eeabe7442b6bbf23f083
Lucas <franclucas@gmail.com>
2023-06-09 13:01:03 -0300

    mudança de host

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -7,7 +7,7 @@ export const TOKEN = "token";
 const baseURL = process.env.REACT_APP_API_URL;
 
 export const httpClient = axios.create({
-    baseURL:'http://localhost:8080/api',
+    baseURL:'https://stunning-tulumba-1e91a4.netlify.app/api',
     withCredentials: true,
 });
 
